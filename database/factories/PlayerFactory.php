6ffb2498754d7eccc46ad5a1381e13140d97112e
Deluze <bartramaekers99@gmail.com>
2019-06-02 23:13:07 +0200

    Fix 2 user rows after db seeding

--- database/factories/PlayerFactory.php
+++ database/factories/PlayerFactory.php
@@ -6,7 +6,6 @@ use Faker\Generator as Faker;
 
 $factory->define(Player::class, function (Faker $faker) {
     return [
-        'user_id'           => factory(User::class)->create()->getKey(),
         'name'              => $faker->firstName,
         'default_character' => 333,
         'rank'              => 1,
